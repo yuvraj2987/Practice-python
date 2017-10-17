f8fb8a31f8d6242c7c375070b56e003bdca884d5
Amit Kulkarni <amitk@wepay.com>
2017-10-17 09:28:29 -0700

    Add linked list class

---
+++ linked_list/list.py
@@ -0,0 +1,27 @@
+
+class Node(object):
+    __slots__ = ["data", "next"]
+    def __init__(self, d):
+        self.next = None
+        self.data = d
+# end of class Node
+
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next is not None:
+                cur = cur.next
+            # end of while
+            cur.next = new_node
+        # end of if else
+    # end of push
+
